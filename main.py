cc8ce15b7311682df3568b873e4b6e924f35f9f5
Alexandt <dgv17022005@gmail.com>
2024-12-23 16:28:35 +0500

    Update main.py

--- main.py
+++ main.py
@@ -4,11 +4,24 @@ from dotenv import load_dotenv
 import os
 
 
-def is_shorten_link(url):
-   parsed = urlparse(url)
+def is_shorten_link(token, url):
+    parsed_url = urlparse(url)
+    if parsed_url.netloc != 'vk.cc':
+        return False
 
-   return parsed.netloc == 'vk.cc'
+    params = {
+        'access_token': token,
+        'url': url,
+        'v': '5.199'
+    }
 
+    response = requests.get(SHORT_URL, params)
+    response.raise_for_status()
+    short_link = response.json()
+
+    if 'error' in short_link['error']:
+        return False
+    return True
 
 def shorten_link(token, link):
     params = {
@@ -25,10 +38,10 @@ def shorten_link(token, link):
 
 
 def count_clicks(token, link):
-    parsed = urlparse(link)
+    parsed_url = urlparse(link)
     params = {
        'access_token': token,
-       'key': parsed.path[1:],
+       'key': parsed_url.path[1:],
        'v': '5.199',
        'interval': 'forever'
     }
@@ -40,15 +53,22 @@ def count_clicks(token, link):
     return counts_link
 
 
-def main():
+if __name__ == "__main__":
+
+    dotenv_path = os.path.join('API', 'token.env')
+    load_dotenv(dotenv_path=dotenv_path)
+    VK_TOKEN = os.environ['TOKEN']
+    SHORT_URL = 'https://api.vk.ru/method/utils.getShortLink'
+    COUNT_URL = 'https://api.vk.ru/method/utils.getLinkStats'
+
     user_input = input('Введите ссылку: ')
 
     try:
-        if is_shorten_link(user_input):
-            clicks_count = count_clicks(TOKEN, user_input)
+        if is_shorten_link(VK_TOKEN, user_input):
+            clicks_count = count_clicks(VK_TOKEN, user_input)
             print('Количество кликов по ссылке: ', clicks_count)
         else:
-            short_link = shorten_link(TOKEN, user_input)
+            short_link = shorten_link(VK_TOKEN, user_input)
             print('Сокращённая ссылка: ', short_link)
     except requests.exceptions.HTTPError:
         print('Произошла ошибка при работе с API')
@@ -56,11 +76,3 @@ def main():
         print('Прозошла ошибка при обработке данных')
     except IndexError:
         print('Произошла ошибка при обработке данных')
-
-
-if __name__ == "__main__":
-    load_dotenv(dotenv_path='API/token.env')
-    TOKEN = os.environ['TOKEN']
-    SHORT_URL = 'https://api.vk.ru/method/utils.getShortLink'
-    COUNT_URL = 'https://api.vk.ru/method/utils.getLinkStats'
-    main()
